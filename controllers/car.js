c14ee283f62daf4a0125840dec67401276c6e661
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-11-17 20:42:00 -0600

    changes made

--- controllers/car.js
+++ controllers/car.js
@@ -1,62 +1,120 @@
-var car = require('../models/car'); 
- 
-// List of all car
-// List of all car 
-exports.car_list = async function(req, res) { 
-    try{ 
-        thecar = await car.find(); 
-        res.send(thecar); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-};  
+var car = require('../models/car');
 
- // VIEWS 
+// List of all car 
+exports.car_list = async function (req, res) {
+    try {
+        thecar = await car.find();
+        res.send(thecar);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// VIEWS 
 // Handle a show all view 
-exports.car_view_all_Page = async function(req, res) { 
-    try{ 
-        thecar = await car.find(); 
-        res.render('car', { title: 'car Search Results', results: thecar }); 
-    } 
-    catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
-// for a specific car. 
-exports.car_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: car detail: ' + req.params.id); 
-}; 
- 
-// Handle Costume create on POST. 
-exports.car_create_post = async function(req, res) { 
-    console.log(req.body) 
-    let document = new car(); 
+exports.car_view_all_Page = async function (req, res) {
+    try {
+        thecar = await car.find();
+        res.render('car', { title: 'car Search Results', results: thecar });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// for a specific car.  
+exports.car_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await car.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
+
+// Handle car create on POST. 
+exports.car_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new car();
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
-    document.shade = req.body.shade; 
-    document.cost = req.body.cost; 
-    document.size = req.body.size; 
+    // {"car_type":"goat", "cost":12, "size":"large"} 
+    document.shade = req.body.shade;
+    document.cost = req.body.cost;
+    document.size = req.body.size;
+    try {
+        let result = await document.save();
+        res.send(result);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle car delete on DELETE. 
+exports.car_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await car.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+
+// Handle car update form on PUT. 
+exports.car_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await car.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.shade) toUpdate.shade = req.body.shade;
+        if (req.body.cost) toUpdate.cost = req.body.cost;
+        if (req.body.size) toUpdate.size = req.body.size;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`);
+    }
+};
+
+// Handle a show one view with id specified by query 
+exports.car_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
     try{ 
-        let result = await document.save(); 
-        res.send(result); 
+        result = await car.findById( req.query.id) 
+        res.render('cardetail',  
+{ title: 'car Detail', toShow: result }); 
     } 
     catch(err){ 
-        res.status(500); 
-        res.send(`{"error": ${err}}`); 
-    }   
-}; 
- 
-// Handle car delete form on DELETE. 
-exports.car_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: car delete DELETE ' + req.params.id); 
-}; 
- 
-// Handle car update form on PUT. 
-exports.car_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: car update PUT' + req.params.id); 
-}; 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle building the view for creating a car. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.car_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('carcreate', { title: 'car Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
--- routes/resource.js
+++ routes/resource.js
@@ -1,32 +1,34 @@
-var express = require('express'); 
-var router = express.Router(); 
- 
+var express = require('express');
+var router = express.Router();
+
 // Require controller modules. 
-var api_controller = require('../controllers/api'); 
-var car_controller = require('../controllers/car'); 
- 
+var api_controller = require('../controllers/api');
+var car_controller = require('../controllers/car');
+
 /// API ROUTE /// 
- 
+
 // GET resources base. 
-router.get('/', api_controller.api); 
-router.get('/car', car_controller.car_list); 
- 
+router.get('/', api_controller.api);
+router.get('/car', car_controller.car_list);
+
 /// car ROUTES /// 
- 
+
 // POST request for creating a car.  
-router.post('/car', car_controller.car_create_post); 
- 
-// DELETE request to delete car. 
-router.delete('/car/:id', car_controller.car_delete); 
- 
+router.post('/car', car_controller.car_create_post);
+
+
+// Delete request to delete car. 
+router.delete('/car/:id',car_controller.car_delete);
+
+
 // PUT request to update car. 
-router.put('/car/:id', 
-car_controller.car_update_put); 
- 
+router.put('/car/:id',
+    car_controller.car_update_put);
+
 // GET request for one car. 
-router.get('/car/:id', car_controller.car_detail); 
- 
+router.get('/car/:id', car_controller.car_detail);
+
 // GET request for list of all car items. 
 
- 
-module.exports = router; 
+
+module.exports = router;
