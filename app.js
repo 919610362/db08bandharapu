13b039ff51edb4cd9a3958b8e7f7f66fc063c40b
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-11-19 00:48:11 -0600

    added

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy; 
 
 const connectionString = 'mongodb+srv://ManishBandharapu:manish123@cluster0.gedt4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 mongoose = require("mongoose");
@@ -65,7 +66,34 @@ var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  } 
+))
 
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/car.js
+++ routes/car.js
@@ -3,13 +3,14 @@ var router = express.Router();
 
 const car_controlers = require('../controllers/car')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('car', { title: 'Search Results Car' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('car', { title: 'Search Results Car' });
+// });
 
 /* GET detail car page */ 
 router.get('/detail', car_controlers.car_view_one_Page);
+router.get('/', car_controlers.car_view_all_Page);
 
 /* GET create car page */ 
 router.get('/create', car_controlers.car_create_Page); 
