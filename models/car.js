9c94c3528181bf534a983033aca125f5fd53d737
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-12-01 17:59:04 -0600

    added bonus

--- models/car.js
+++ models/car.js
@@ -1,8 +1,14 @@
-const mongoose = require("mongoose") 
-const carSchema = mongoose.Schema({ 
- shade: String, 
- cost: Number, 
- size: String 
-}) 
- 
-module.exports = mongoose.model("car", carSchema) 
+const mongoose = require("mongoose")
+const carSchema = mongoose.Schema({
+    shade: {
+        type: String,
+        minLength: 45
+    },
+    cost: {
+        type: Number,
+        minLength: 99
+    },
+    size: String
+})
+
+module.exports = mongoose.model("car", carSchema)
