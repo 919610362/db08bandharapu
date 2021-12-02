d702a4787eaa900d4dafe2b123ad2f8d01e53fb9
Manish Goud Bandharapu <manishgoud101@gmail.com>
2021-12-01 18:00:08 -0600

    added bonus

--- routes/car.js
+++ routes/car.js
@@ -13,12 +13,12 @@ router.get('/detail', car_controlers.car_view_one_Page);
 router.get('/', car_controlers.car_view_all_Page);
 
 /* GET create car page */ 
-router.get('/create', car_controlers.car_create_Page); 
+router.get('/create', secured, car_controlers.car_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', car_controlers.car_update_Page); 
+router.get('/update', secured,car_controlers.car_update_Page); 
 
 /* GET create car page */ 
-router.get('/delete', car_controlers.car_delete_Page); 
+router.get('/delete', secured,car_controlers.car_delete_Page); 
 
 module.exports = router;
